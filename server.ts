2f6ad90fe34c9868f1be74232670f0c7574f4762
tomiwa <sannimichaelse@gmail.com>
2020-12-19 20:08:34 +0100

    [ch-176234341] Update README.md

--- server.ts
+++ server.ts
@@ -9,6 +9,15 @@ const app: Application = express();
 const server: Server = new Server(app);
 const PORT = config.PORT || 3002;
 
+const API_URL: string = config.API_URL;
+const API_KEY_URL: string = config.API_KEY;
+
+if (!API_KEY_URL || !API_KEY_URL) {
+  console.log('Please create .env file, refer .env.sample');
+  process.exit(0);
+}
+
+
 app.listen(PORT, () => {
   console.log('ok, server is running');
 });
