cd2adbd0d20cc2a4845e9357c374284db2d00b50
tomiwa <sannimichaelse@gmail.com>
2020-12-19 07:17:08 +0100

    [ch-#176232564] remove unncessary switch condition

--- src/handlers/api-responses.ts
+++ src/handlers/api-responses.ts
@@ -7,7 +7,7 @@
       return {
         status: "error",
         message,
-        code: this.getStatusCode(code),
+        code,
         data,
       };
     }
@@ -16,7 +16,7 @@
       return {
         status: "success",
         message,
-        code: this.getStatusCode(code),
+        code,
         data,
       };
     }
@@ -28,44 +28,7 @@
         code: HttpStatus.OK,
       };
     }
-  
-    getStatusCode(code) {
-      let statusCode;
-      switch (code) {
-        case 200:
-          statusCode = HttpStatus.OK;
-          break;
-        case 201:
-          statusCode =HttpStatus.CREATED;
-          break;
-        case 401:
-          statusCode = HttpStatus.UNAUTHORIZED;
-          break;
-        case 403:
-          statusCode = HttpStatus.FORBIDDEN;
-          break;
-        case 400:
-          statusCode = HttpStatus.BAD_REQUEST;
-          break;
-        case 404:
-          statusCode = HttpStatus.NOT_FOUND;
-          break;
-        case 409:
-          statusCode = HttpStatus.CONFLICT;
-          break;
-        case 410:
-          statusCode = HttpStatus.GONE;
-          break;
-        case 422:
-          statusCode = HttpStatus.UNPROCESSABLE_ENTITY;
-          break;
-        case 500:
-          statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
-          break;
-      }
-  
-      return statusCode;
-    }
+
   }
   
   export default new ApiResponse();
